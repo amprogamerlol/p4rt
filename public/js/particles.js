1764f81eee779ec1ccdd9c5c6c3750330b06fe09
IIsoki <wmanthe3rd@gmail.com>
2024-08-16 20:07:31 -0700

    Update particles.js

--- public/js/particles.js
+++ public/js/particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   particles: {
     number: { value: 160, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
+    color: { value: "#000000" },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
@@ -11,23 +11,23 @@ particlesJS("particles-js", {
     opacity: {
       value: 1,
       random: true,
-      anim: { enable: false, speed: 1, opacity_min: 0, sync: false },
+      anim: { enable: false, speed: 10, opacity_min: 0, sync: false },
     },
     size: {
       value: 3,
       random: true,
-      anim: { enable: false, speed: 4, size_min: 0.3, sync: false },
+      anim: { enable: false, speed: 10, size_min: 0.3, sync: false },
     },
     line_linked: {
       enable: false,
       distance: 150,
-      color: "#ffffff",
+      color: "#000000",
       opacity: 0.4,
       width: 1,
     },
     move: {
       enable: true,
-      speed: 1,
+      speed: 10,
       direction: "none",
       random: true,
       straight: false,
